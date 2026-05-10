77ecd5013e1f3660e5fc7f611c0fc81ff64ad175
Ganeshchennuboina <ganeshchennuboina62@gmail.com>
2026-05-10 16:47:11 +0530

    updated

--- src/com/Example1.java
+++ src/com/Example1.java
@@ -1,5 +1,9 @@
 package com;
 
 public class Example1 {
+public static void main(String[] args) {
+	System.out.println("my name  is ganesh yadav chennuboina");
+	System.out.println("ganesh yadav chennuboina");
+}
 
 }
